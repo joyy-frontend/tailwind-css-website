96f8283f45ee15f02013cf60b10af57df95826f4
aurorar17 <aurora.roilo01@gmail.com>
2024-09-25 20:45:06 -0700

    aurora2part

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#23A6F0",
+        "light-blue-color": "#8EC2F2",
+        "dark-background-color": "#252B42",
+        "light-text-color": "#FFFFFF",
+        "second-text-color": "#737373",
+      },
+    },
+  },
+  plugins: [],
+};
